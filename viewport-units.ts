6c6bea9528f7b4313681d5158100c29693b0be32
Yusuke Kamiyamane <y@y14e.com>
2026-09-16 12:38:05 +0900

    Update viewport-units.ts

--- viewport-units.ts
+++ viewport-units.ts
@@ -38,7 +38,7 @@ export function updateViewportUnits(
   );
   const { style } = root;
 
-  function step(): void {
+  function update(): void {
     timer = undefined;
     const { clientWidth: w, clientHeight: h } = html;
     const vw = w / 100;
@@ -65,7 +65,7 @@ export function updateViewportUnits(
 
   function onResize(): void {
     if (timer === undefined) {
-      timer = requestAnimationFrame(step);
+      timer = requestAnimationFrame(update);
     }
   }
 
